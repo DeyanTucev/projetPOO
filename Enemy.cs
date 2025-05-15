b89d85ede9f9e7fb4d67bb5d651aa968f6b73b3c
Chris <pl52dsy@eduvaud.ch>
2025-05-15 23:03:00 +0200

    avancement de l'implémentation des ennemis

--- Enemy.cs
+++ Enemy.cs
@@ -3,4 +3,30 @@ using System;
 
 public partial class Enemy : Area2D
 {
+    [Export] public PackedScene BulletScene;
+    [Export] public float BulletSpeed = 200f;
+
+    public void Shoot()
+    {
+        if (BulletScene != null)
+        {
+            GD.Print("BulletScene not assigned.");
+            return;
+        }
+
+        var bullet = BulletScene.Instantiate<Node2D>();
+        bullet.GlobalPosition = GlobalPosition;
+
+        GetTree().Root.AddChild(bullet);
+    }
+
+    private void _on_Area2D_area_entered(Area2D area)
+    {
+
+        if (area.IsInGroup("PlayerBullet"))
+        {
+            QueueFree();
+            area.QueueFree();
+        }
+    }
 }
