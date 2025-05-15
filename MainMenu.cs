308d4e0da8cd588e9ac38c213432929991787806
Samuel Gergely <143069755+SamuelGergely@users.noreply.github.com>
2025-05-15 16:28:08 +0200

    menu

---
+++ MainMenu.cs
@@ -0,0 +1,67 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	private Control settingsMenu;
+	private Control creditsMenu;
+	private Control mainButtons;
+	
+	public override void _Ready()
+	{
+		settingsMenu = GetNode<Control>("CenterContainer/SettingsMenu");
+		creditsMenu = GetNode<Control>("CenterContainer/CreditsMenu");
+		mainButtons = GetNode<Control>("CenterContainer/MainButtons");
+		
+		GetNode<Button>("CenterContainer/SettingsMenu/back").Pressed += CloseSettings;
+		GetNode<Button>("CenterContainer/CreditsMenu/back").Pressed += CloseCredits;
+		
+		settingsMenu.Visible = false;
+		creditsMenu.Visible = false;
+		
+		GetNode<Button>("CenterContainer/MainButtons/play").Pressed += OnPlayPressed;
+		GetNode<Button>("CenterContainer/MainButtons/settings").Pressed += OnSettingsPressed;
+		GetNode<Button>("CenterContainer/MainButtons/credits").Pressed += OnCreditsPressed;
+		GetNode<Button>("CenterContainer/MainButtons/quit").Pressed += OnQuitPressed;
+	}
+	
+	private void OnPlayPressed()
+	{
+		GD.Print("Lancement du jeu...");
+		GetTree().ChangeSceneToFile("res://player.tscn");
+	}
+	
+	private void OnSettingsPressed()
+	{
+		GD.Print("Ouverture des paramètres...");
+		settingsMenu.Visible = true;
+		creditsMenu.Visible = false;
+		mainButtons.Visible = false;
+	}
+	
+	private void OnCreditsPressed()
+	{
+		GD.Print("Affichage des crédits...");
+		settingsMenu.Visible = false;
+		creditsMenu.Visible = true;
+		mainButtons.Visible = false;
+	}
+	
+	private void OnQuitPressed()
+	{
+		GD.Print("Quitter le jeu...");
+		GetTree().Quit();
+	}
+	
+	public void CloseSettings()
+	{
+		settingsMenu.Visible = false;
+		mainButtons.Visible = true;
+	}
+
+	public void CloseCredits()
+	{
+		creditsMenu.Visible = false;
+		mainButtons.Visible = true;
+	}
+}
--- player.cs
+++ player.cs
@@ -1,30 +1,30 @@
 using Godot;
 using System;
 
-public partial class player : Area2D
+public partial class Player : Area2D
 {
-    [Export]
-    public int Speed { get; set; } = 10;
+	[Export]
+	public int Speed { get; set; } = 10;
 
-    public Vector2 ScreenSize;
+	public Vector2 ScreenSize;
 
-    public override void _Ready()
-    {
-        ScreenSize = GetViewport().GetVisibleRect().Size;
-    }
+	public override void _Ready()
+	{
+		ScreenSize = GetViewport().GetVisibleRect().Size;
+	}
 
-    public override void _Process(double delta)
-    {
+	public override void _Process(double delta)
+	{
 
-        int movement = 10;
-        if (Input.IsKeyPressed(Key.A) || Input.IsKeyPressed(Key.Left))
-        {
-            Position += new Vector2(-movement, 0);
-        }
-        if (Input.IsKeyPressed(Key.D) || Input.IsKeyPressed(Key.Right))
-        {
-            Position += new Vector2(movement, 0);
-        }
-        
-    }
+		int movement = 10;
+		if (Input.IsKeyPressed(Key.A) || Input.IsKeyPressed(Key.Left))
+		{
+			Position += new Vector2(-movement, 0);
+		}
+		if (Input.IsKeyPressed(Key.D) || Input.IsKeyPressed(Key.Right))
+		{
+			Position += new Vector2(movement, 0);
+		}
+		
+	}
 }
